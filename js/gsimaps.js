7ea55e51ab23aad7098aa5b6b89699c82127caa5
wata909 <wata909@gmail.com>
2015-02-26 15:27:42 +0900

    add tms layers

--- js/gsimaps.js
+++ js/gsimaps.js
@@ -23,8 +23,7 @@ CONFIG.layers = [
 	'./layers_txt/layers2.txt',
 	'./layers_txt/layers3.txt',
 	'./layers_txt/layers4.txt',
-	'./layers_txt/layers5.txt',
-	'./layers_txt/layers6.txt'
+	'./layers_txt/layers5.txt'
 ];
 
 
